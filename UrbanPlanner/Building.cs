8d87d7f25f81e2d6aa9a445fd7b9d0ed1a8053ff
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-07-24 10:11:13 -0500

    finished

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        private string _designer = "Lindsey Satterfield";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string ownerName)
+        {
+            _owner = ownerName;
+            Console.WriteLine(_address);
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            FiveOneTwoEigth.Width = 500.5;
+            FiveOneTwoEigth.Depth = 1000.5;
+            FiveOneTwoEigth.Stories = 20;
+            FiveOneTwoEigth.Construct();
+            FiveOneTwoEigth.Purchase("Lindsey Satterfield");
+            Console.WriteLine();
+
+            Building BatmanBuilding = new Building("333 Commerce St");
+            BatmanBuilding.Width = 800.6;
+            BatmanBuilding.Depth = 300.4;
+            BatmanBuilding.Stories = 33;
+            BatmanBuilding.Construct();
+            BatmanBuilding.Purchase("Batman");
+            Console.WriteLine();
+
+            Building EmpireStateBuilding = new Building("350 Fifth Avenue");
+            EmpireStateBuilding.Width = 700;
+            EmpireStateBuilding.Depth = 1250;
+            EmpireStateBuilding.Stories = 102;
+            EmpireStateBuilding.Construct();
+            EmpireStateBuilding.Purchase("Some New Yorker");
+            Console.WriteLine();
+
+            Building BridgestoneArena = new Building("501 Broadway");
+            BridgestoneArena.Width = 900;
+            BridgestoneArena.Depth = 478;
+            BridgestoneArena.Stories = 7;
+            BridgestoneArena.Construct();
+            BridgestoneArena.Purchase("Nashville Predators");
+            Console.WriteLine();
+        }
+    }
+}
